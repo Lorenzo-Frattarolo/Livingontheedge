a54f3ea936a7fba1fb27069049f8a66e769ad23c
Frattalol <lorenzo.frattarolo@gmail.com>
2021-12-13 14:25:56 +0100

    Update QMCsob_Estimator.m

--- MC example Matlab/QMCsob_Estimator.m
+++ MC example Matlab/QMCsob_Estimator.m
@@ -1,7 +1,6 @@
 %Quasi-Monte Carlo
 function mu_hat = QMCsob_Estimator(ndim,f,n,q)
 p = sobolset(ndim);
-p = scramble(p,'MatousekAffineOwen');
 q = qrandstream(p);
 x_vals=qrand(q,n);
 f_vals =f(x_vals);
